48ce8edb9f77aae0887f46e657db3e9d27f5d9c2
Jason Chafin <1000543+Herm71@users.noreply.github.com>
2026-02-09 09:39:28 -0800

    adjust render_liquid.rb

--- source/_plugins/render_liquid.rb
+++ source/_plugins/render_liquid.rb
@@ -7,7 +7,8 @@ module Jekyll
 
     def absolutify_images(input)
       return input if input.nil? || input.empty?
-      site_url = @context.registers[:site].config['url']
+      site_config = @context.registers[:site].config
+      site_url = site_config['url']
       # Convert relative image src to absolute URLs
       input.gsub(/src="\/assets\//, "src=\"#{site_url}/assets/")
           .gsub(/src='\/assets\//, "src='#{site_url}/assets/")
